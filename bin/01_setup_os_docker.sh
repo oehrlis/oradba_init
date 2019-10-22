cf23cb805d186d14b37f5c2c24d311aa8a2ade02
Stefan Oehrli <stefan.oehrli@trivadis.com>
2019-10-22 16:00:36 +0200

    add 01_setup_os_docker.sh

---
+++ bin/01_setup_os_docker.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+# ---------------------------------------------------------------------------
+# Trivadis AG, Infrastructure Managed Services
+# Saegereistrasse 29, 8152 Glattbrugg, Switzerland
+# ---------------------------------------------------------------------------
+# Name.......: 01_setup_os_docker.sh 
+# Author.....: Stefan Oehrli (oes) stefan.oehrli@trivadis.com
+# Editor.....: Stefan Oehrli
+# Date.......: 2019.10.22
+# Revision...: 
+# Purpose....: Script to configure OEL for Docker.
+# Notes......: Script would like to be executed as root :-).
+# Reference..: --
+# License....: Licensed under the Universal Permissive License v 1.0 as 
+#              shown at http://oss.oracle.com/licenses/upl.
+# ---------------------------------------------------------------------------
+# Modified...:
+# see git revision history for more information on changes/updates
+# ---------------------------------------------------------------------------
+
+# - Environment Variables ---------------------------------------------------
+# source genric environment variables and functions
+source "$(dirname ${BASH_SOURCE[0]})/00_setup_oradba_init.sh"
+
+# define oradba specific variables
+export ORADBA_BIN="$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)"
+export ORADBA_BASE="$(dirname ${ORADBA_BIN})"
+export ORADBA_RSP="${ORADBA_BASE}/rsp"          # oradba init response file folder
+
+# define Oracle specific variables
+export ORACLE_ROOT=${ORACLE_ROOT:-"/u00"}       # root folder for ORACLE_BASE and binaries
+export ORACLE_DATA=${ORACLE_DATA:-"/u01"}       # Oracle data folder eg volume for docker
+export ORACLE_ARCH=${ORACLE_ARCH:-"/u02"}       # Oracle arch folder eg volume for docker
+export ORACLE_BASE=${ORACLE_BASE:-"${ORACLE_ROOT}/app/oracle"}
+export ORACLE_INVENTORY=${ORACLE_INVENTORY:-"${ORACLE_ROOT}/app/oraInventory"}
+
+# define generic variables for software, download etc
+export OPT_DIR=${OPT_DIR:-"/opt"}
+export SOFTWARE=${SOFTWARE:-"${OPT_DIR}/stage"} # local software stage folder
+export DOWNLOAD=${DOWNLOAD:-"/tmp/download"}    # temporary download location
+export CLEANUP=${CLEANUP:-true}                 # Flag to set yum clean up
+export YUM="yum"
+export DEFAULT_PASSWORD=${default_password:-"LAB01schulung"}
+# - EOF Environment Variables -----------------------------------------------
+
+# Make sure only root can run our script
+if [ $EUID -ne 0 ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+echo 'Installing and configuring Docker engine'
+
+# install Docker engine
+yum -y install docker-engine
+
+# Format spare device as Btrfs
+# Configure Btrfs storage driver
+echo "docker-storage-config -s btrfs -d /dev/sdb"
+
+# Start and enable Docker engine
+systemctl start docker
+systemctl enable docker
+
+# Add vagrant user to docker group
+usermod -a -G docker oracle
+
+# Relax /etc/docker permissions (vagrant-proxyconf maintains system-wide config)
+chmod a+x /etc/docker
+
+echo 'Docker engine is ready to use'
+echo
+# --- EOF --------------------------------------------------------------------
